ea6a763dfaadbea2d376aa2cc9d48ce26647977d
dhanur350 <dhanur297@gmail.com>
2022-09-17 14:52:39 +0530

    Day2

--- Practice/script.js
+++ Practice/script.js
@@ -1,4 +1,6 @@
 let name1 = "Dhanur123"
 // document.write(name);
 let section = document.getElementsByClassName("sct")[0];
+let sector = document.getElementById("sctt");
+sector.innerText = "Hello buddy"
 section.innerText = name1;
