9ecdd3c50a128951e831da38b73f5a5396fa6c63
Amandeep <amandeepbisht123@gmail.com>
2023-01-04 19:04:45 +0530

    4jan2023

---
+++ Amandeep/oops/insertattail.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertathead(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head != NULL)
+    {
+        n->next = head;
+    }
+    head = n;
+}
+
+void insertattail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void deleteathead(node* &head){
+    node* todelete=head;
+    head=head->next;
+    delete todelete;
+}
+
+void deletion(node* &head,int val){
+    node* temp=head;
+    if(head==NULL){
+        return;
+    }
+    if(head->next==NULL){
+        deleteathead(head);
+        return;
+    }
+    while(temp->next->data!=val){
+        temp=temp->next;
+    }
+    node* todelete=temp->next;
+    temp->next=temp->next->next;
+    delete todelete;
+}
+
+void display(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertattail(head, 1);
+    insertattail(head, 2);
+    insertattail(head, 3);
+    insertattail(head, 4);
+    display(head);
+    // insertathead(head,10);
+    // display(head);
+    deletion(head,2);
+    display(head);
+    return 0;
+}
